a637d5dbdefc5da9014b35f37ae7db8cd9319cf9
Ilya Eletskiy <ilarelet@gmail.com>
2022-09-24 15:49:23 +0300

    Console game now working

--- main.js
+++ main.js
@@ -5,24 +5,39 @@ list = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     // randomly generate a number a rount it up to 1, 2 or 3
     numChosen = Math.floor(Math.random()*3);
-    console.log(list[numChosen]);
+    return(list[numChosen]);
 }
 
 //the next function determines who won and prints out the result
 function playGame(playerSelection, computerSelection){
     //if it's a tie
     if (playerSelection===computerSelection){
-        console.log(`Oh WOW! it's a tie! Both sides chose ${playerSelection}.`)
+        console.log(`Oh WOW! it's a tie! Both sides chose ${playerSelection}.`);
     }
     // when the player wins:
     else if ((playerSelection==="rock" && computerSelection==="scissors")||
     (playerSelection==="paper" && computerSelection==="rock")||
     (playerSelection==="scissors" && computerSelection==="paper")){
-        console.log(`Congratulations! You won! ${playerSelection} beats ${computerSelection}.`)
+        console.log(`Congratulations! You won! ${playerSelection} beats ${computerSelection}.`);
     }
     else{
     //when the computer wins:
-        console.log(`Sorry, you lost! ${computerSelection} beats ${playerSelection}.`)
+        console.log(`Sorry, you lost! ${computerSelection} beats ${playerSelection}.`);
     }
 }
 
+function play(playerSelection){
+    let playerChoice = "";
+    for (let choices in list) {
+        if (list[choices] === playerSelection.toLowerCase()){
+            playerChoice=list[choices];
+        }
+    }
+    if (playerChoice === ""){
+        console.log("Your choice is not a possible option. Try again: ");
+        return null;
+    }
+    playGame(playerChoice, getComputerChoice());
+}
+
+console.log(`To play text "play(*your choice*)"`);
