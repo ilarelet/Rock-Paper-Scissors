194da487c9b297bb5487cbd789c20a60b65e2335
Ilya Eletskiy <ilarelet@gmail.com>
2022-10-13 14:37:35 +0300

    Displaying the score and results as DOMs

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ list = ["rock", "paper", "scissors"];
 
 //Function defining what the computer chooses:
 function getComputerChoice() {
-    // randomly generate a number a rount it up to 1, 2 or 3
+    // randomly generate a number and round it up to 1, 2 or 3
     numChosen = Math.floor(Math.random()*3);
     return(list[numChosen]);
 }
@@ -30,57 +30,51 @@ function whoWon(playerSelection, computerSelection){
 function resultMsg(gameResult, playerSelection, computerSelection){
     //if it's a tie
     if (gameResult==='tie'){
-        console.log(`It's a tie! Both sides chose ${playerSelection}. Try again.`);
+        return(`It's a tie! Both sides chose ${playerSelection}. Try again.`);
     }
     // when the player wins:
     else if (gameResult==='win'){
-        console.log(`You won! ${playerSelection} beats ${computerSelection}.`);
+        return(`You won! ${playerSelection} beats ${computerSelection}.`);
     }
     else{
     //when the computer wins:
-        console.log(`You lost! ${computerSelection} beats ${playerSelection}.`);
+        return(`You lost! ${computerSelection} beats ${playerSelection}.`);
     }
 }
 
-function play(){
-    scoreComputer = 0;
-    scorePlayer = 0;
-    for (let i=0; i<5; i++){  // playing the game times
-        let playerSelection = prompt("Enter your choice: "); //prompting players choice as a string
-        let playerChoice = "";   // that var will have a correctly spelled choice from the list
-        for (let choices in list) {  // checking if the entered selection is in the list
-            if (list[choices] === playerSelection.toLowerCase()){
-                playerChoice=list[choices];  
-            }
-        }
-        if (playerChoice === ""){  // if the selection is incorrect that variaable will stay empty
-            console.log("Your choice is not a possible option. Try again: ");
-            i=i-1; //if we do not play we stay on the same round
-            continue;
-        };
-        // generate the choice for the computer
-        computerChoice = getComputerChoice();
-        // determining who won
-        gameResult=whoWon(playerChoice, computerChoice);
-        //updating the scores:
-        if (gameResult==='win') {scorePlayer++}
-        else if (gameResult==='lose'){scoreComputer++}
-        else {
-            i=i-1; //if it is a tie the round doesn't count
-        };
-        //print out the results
-        resultMsg(gameResult, playerChoice, computerChoice);
-        console.log(`Current score is: ${scorePlayer}-${scoreComputer}`);
-        if (scorePlayer===3 || scoreComputer===3){
-            return(scorePlayer, scoreComputer);
-        }
-    }
-    return(scorePlayer, scoreComputer);
+//main game function that compares player's choice to the computer-generated and returs the updated score
+function play(playerChoice, score){
+    // generate the choice for the computer
+    computerChoice = getComputerChoice();
+    // determining who won
+    gameResult=whoWon(playerChoice, computerChoice);
+    //updating the scores:
+    if (gameResult==='win') {score[0]++}
+    else if (gameResult==='lose'){score[1]++}
+
+
+    //print out the results
+    resultText=document.querySelector('#roundResult')
+    scoreText = document.querySelector('#currentScore')
+    resultText.textContent = resultMsg(gameResult, playerChoice, computerChoice);
+    scoreText.textContent=`Current score is: ${score[0]}-${score[1]}`;
+    return([score]); 
 }
 
+//initate the original score: 0:0
 let scorePlayer = 0;
 let scoreComputer = 0;
-scorePlayer, scoreComputer = play();
+score = [scorePlayer, scorePlayer];
+
+//if we press a button the game starts with a corresponding playerChoice
+playButtons = document.querySelectorAll('.playerChoice');
+playButtons.forEach((playButton) => 
+    playButton.addEventListener('click', function(e) {
+        play(e.target.getAttribute('data-choice'), score);
+}));
+
+
+/*
 //print out final message:
 let resultString = 
     (scorePlayer>scoreComputer) 
@@ -88,4 +82,4 @@ let resultString =
     : "Sorry, you lost the game!";
 
 alert(resultString+`\nFinal score is: ${scorePlayer}-${scoreComputer}\nWanna try again?`);
-location.reload();
+location.reload();*/
